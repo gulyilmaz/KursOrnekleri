ab54fa66cc214aaecb53933b672f259d62e9dd95
gulyilmaz <50866390+gulyilmaz@users.noreply.github.com>
2021-12-26 21:48:30 +0300

    Delete Oop-23-12-2021 directory

--- Oop-23-12-2021/20_05_ClassLibrary/20_05_ClassLibrary/Insan.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _20_05_ClassLibrary
-{
- class Insan : Canli
- {
-  public string Ad { get; set; }
-  public string Soyad { get; set; }
-  public string tcNo { get; set; }
- }
-}
--- Oop-23-12-2021/20_05_ClassLibrary/20_05_ClassLibrary/Personel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace _20_05_ClassLibrary
-{//Birçok sınıfı bir arada barındırır.Build edildiğinde dll projesi oluşturur
- class Personel : Insan
- {
-  public string sgkNo { get; set; }
-  public double maasi { get; set; }
- }
-}
